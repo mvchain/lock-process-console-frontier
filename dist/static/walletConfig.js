9f317de2ac61e7cb0762967b828a04317b842848
ningjianhao <375332835@qq.com>
2018-01-20 14:59:33 +0800

    delay

--- dist/static/walletConfig.js
+++
@@ -1,7 +0,0 @@
-
-const urlData = {
-  url: 'http://192.168.201.22:8765/api',
-  type: 'NJH',
-  coin: 'MVC'
-}
-window.urlData = urlData
--- src/api/dataCenter.js
+++
@@ -1,8 +0,0 @@
-import request from '@/utils/request'
-
-export function getData(data) {
-  return request({
-    url: '/ethereum/ethereum/all_balance' + data,
-    method: 'get'
-  })
-}
